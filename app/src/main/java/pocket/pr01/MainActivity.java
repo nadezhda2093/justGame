fd7c87736d0e9ef7fcfea43a8e0ff51946d6acaf
NGavrilova <gavrilova_179@mail.ru>
2020-03-04 18:47:29 +0400

    Установка ADB

---
+++ app/src/main/java/pocket/pr01/MainActivity.java
@@ -0,0 +1,14 @@
+package pocket.pr01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
